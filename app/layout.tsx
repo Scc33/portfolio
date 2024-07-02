052be4ab25f71ab87eb699375ef04f7092955987
Sean Coughlin <coughlinscc33@gmail.com>
2024-07-01 21:30:06 -0500

    Add my favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,18 +11,21 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Sean Coughlin Portfolio',
+    template: '%s | Sean Coughlin Portfolio',
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
+    title: 'Sean Coughlin Portfolio',
     description: 'This is my portfolio.',
     url: baseUrl,
     siteName: 'My Portfolio',
     locale: 'en_US',
     type: 'website',
   },
+  icons: [
+    { url: '/s.webp', sizes: 'any' },
+  ],
   robots: {
     index: true,
     follow: true,
