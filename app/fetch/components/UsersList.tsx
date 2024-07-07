abbc5bb8db75c8f8f99345821dd59f8c945cff3f
Sean Coughlin <coughlinscc33@gmail.com>
2024-07-07 18:24:18 -0500

    Add data fetching examples

---
+++ app/fetch/components/UsersList.tsx
@@ -0,0 +1,37 @@
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export default async function FetchUsers(): Promise<User[]> {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+    next: { revalidate: 10 },
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const users: User[] = await res.json();
+
+  return users;
+}
---
+++ app/fetch/page.tsx
@@ -0,0 +1,31 @@
+import FetchUsers from "./components/UsersList";
+import { Users } from "lucide-react";
+
+export const metadata = {
+  title: "Fetch Example",
+  description: "Example of NextJS fetching and caching.",
+};
+
+const Page = async () => {
+  const userData = await FetchUsers();
+  return (
+    <div>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+        Data Fetching Server Component
+      </h1>
+      {userData.map((user) => (
+        <div key={user.id} className="p-4 rounded-lg shadow-md mb-4">
+          <h2 className="flex text-lg font-semibold">
+            <Users />
+            {user.name}
+          </h2>
+          <p className="text-sm text-gray-600">{user.email}</p>
+          <p className="text-sm text-gray-600">{user.phone}</p>
+          <p className="text-sm text-gray-600">{user.website}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Page;
