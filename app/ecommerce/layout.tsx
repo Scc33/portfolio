21189babbaab2be65278505b3de4c30b0f72c1e7
Sean Coughlin <coughlinscc33@gmail.com>
2024-07-18 16:22:54 -0500

    Start on drag and drog for ecommerce configure page

--- app/ecommerce/layout.tsx
+++ app/ecommerce/layout.tsx
@@ -1,19 +1,21 @@
-import { Recursive } from 'next/font/google'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import { Recursive } from "next/font/google";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-const recursive = Recursive({ subsets: ['latin'] })
+const recursive = Recursive({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
+    children
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode;
 }>) {
-  return (
-      <div className={recursive.className}>
-        <Navbar />
-        {children}
-        <Footer />
-      </div>
-  )
+    return (
+        <div className={recursive.className}>
+            <Navbar />
+            <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+                <div className="flex-1 flex flex-col h-full">{children}</div>
+                <Footer />
+            </main>
+        </div>
+    );
 }
